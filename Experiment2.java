e2e16ce945c20f6552adde65b862b3c20379d9fe
HimanshuGupta13 <himansshugupta10306@gmail.com>
2026-01-30 19:10:42 +0530

    Add files via upload

---
+++ Experiment2.java
@@ -0,0 +1,26 @@
+class AreaCalculator {
+    public double calculateArea(double radius) {
+        return 3.14 * radius * radius;
+    }
+
+    public double calculateArea(double length, double width) {
+        return length * width;
+    }
+
+    public int calculateArea(int side) {
+        return side * side;
+    }
+}
+public class Experiment2 {
+    public static void main(String[] args) {
+        AreaCalculator calculator = new AreaCalculator();
+        double circleArea = calculator.calculateArea(5.0);
+        System.out.println("Area of Circle: " + circleArea);
+
+        double rectangleArea = calculator.calculateArea(4.0, 6.0);
+        System.out.println("Area of Rectangle: " + rectangleArea);
+
+        int squareArea = calculator.calculateArea(7);
+        System.out.println("Area of Square: " + squareArea);
+    }
+}
